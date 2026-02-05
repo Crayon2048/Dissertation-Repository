57674c6f4d59626cc72d4fee32899d90dfcac8b7
Kleone <kleonegayya12@gmail.com>
2026-02-05 17:25:15 +0000

    Editing code to add Injury Pie Chart

--- FinalPrediction.py
+++ FinalPrediction.py
@@ -222,10 +222,11 @@ print(df_info.head(20))
 #Workload features 
 players = add_player_workload_features(players)
 
-#Feature distributions and Injury Pie Chart
+#Feature distributions
 for c in ["Training_Intensity", "Recovery_Time", "Previous_Injuries", "minutes"]:
     plot_feature_distributions(players, c, target_col="Injury")
 
+# Adding Injury Pie Chart
 counts = players["Injury"].value_counts()
 plt.figure(figsize=(4.2,4.3))
 plt.pie(counts, labels=counts.index, autopct="%1.1f%%", startangle=140,
